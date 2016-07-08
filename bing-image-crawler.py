b386d601a53884d416ccea7123b1fe4425817788
YuxJ173 <yuxj173@gmail.com>
2016-07-08 20:56:36 +0800

    1

--- bing-image-crawler.py
+++ bing-image-crawler.py
@@ -1,5 +1,6 @@
-import sys
-import time
+# 
+# python3 coding
+
 import urllib.request
 from bs4 import BeautifulSoup
 
@@ -8,11 +9,15 @@ domain = 'http://www.istartedsomething.com/bingimages/'
 def single(url):
     page = BeautifulSoup(urllib.request.urlopen(url).read(), 'html.parser')
     for a in page.findAll('a', class_ = 'cn'):
-        print(1)
+        date = a['href'][1:-3]
         name = a.find('img', class_ = 'lazy')['data-original'][13:-6]
+        print('catching %s - %s...'%(date, name), end = '', flush = True)
+        
         img = domain + 'cache/' + name
-        file = open(name, 'wb')
+        file = open(date + '-' + name, 'wb')
         file.write(urllib.request.urlopen(img).read())
         file.close()
+        
+        print('done.')
 
 single(domain)
